bb21e5128d2ada58a1ab5059baf0773d06ffe63b
LiangCheng Wang <zaq14760@gmail.com>
2025-12-03 16:40:33 +0800

    Initial commit: baseline test-framework: scripts utilities
    - Scripts are global utilities not belonging to specific test cases

---
+++ scripts/imx95/smarc-imx95/ismc-cb/about_info.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+dialog --title "About this framework" --msgbox "Universal Test Framework v2.1\n\nSupports:\n- Multi-platform menu.json\n- Dialog-based UI\n- Flow / Script / Info categories\n- Time loop / Parallel testing\n\nAuthor: LCWang" 15 60
+clear
---
+++ scripts/imx95/smarc-imx95/ismc-cb/power_cycle.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+dialog --infobox "Simulating Power Off -> On..." 5 45
+sleep 3
+dialog --msgbox "Power Cycle simulation complete" 5 40
+clear
